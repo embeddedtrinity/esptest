07432f9b06c0a8422d1a95fb36ccc90b8d857522
tx <embedded.trinity@gmail.com>
2026-03-25 11:58:45 +0800

    v2.0

--- main/main.c
+++ main/main.c
@@ -3,7 +3,11 @@
 
 void app_main(void)
 {
-
     gpio_config_t  iostruct={0};
     iostruct.intr_type = GPIO_INTR_DISABLE;
+    iostruct.mode = GPIO_MODE_OUTPUT;
+    iostruct.pin_bit_mask = 1ull << 16;
+    iostruct.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    iostruct.pull_up_en = GPIO_PULLUP_DISABLE;
+    gpio_config(&iostruct);
 }
