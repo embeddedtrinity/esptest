7d91b6a8bff832100946962a079e4d4cd0581fcc
tx <embedded.trinity@gmail.com>
2026-03-25 14:54:11 +0800

    d v1.0

--- main/main.c
+++ main/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "driver/uart.h"
 #include "driver/gpio.h"
 
 void app_main(void)
@@ -10,4 +11,14 @@ void app_main(void)
     iostruct.pull_down_en = GPIO_PULLDOWN_DISABLE;
     iostruct.pull_up_en = GPIO_PULLUP_DISABLE;
     gpio_config(&iostruct);
+    uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
+    };
+    // Set UART pins(TX: IO4, RX: IO5, RTS: IO18, CTS: IO19)
+    ESP_ERROR_CHECK(uart_set_pin(2, 4, 5, 18, 19));
 }
